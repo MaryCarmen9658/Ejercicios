d69d4f4f68a8830529819d3374915be2d2bdeab7
MaryCarmen9658 <mariacarmen_frias@hotmail.com>
2020-11-13 15:41:46 -0600

    Se creo la clase Cuadrado

---
+++ morse/Cuadrado.java
@@ -0,0 +1,21 @@
+package Cuadrado;
+
+public class Cuadrado {
+	public double x;
+
+    public  Cuadrado(double lado) {
+        this.x =  lado;
+    }
+
+
+
+
+    public  double calculateArea(){
+        return this.x * this.x;
+    }
+
+    public  double calculatePerimeter()
+    {
+        return this.x *4;
+    }
+}
